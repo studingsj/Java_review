3609bf005c4ab3e4b483d896935e70f8601ff95b
java_sj <toobi0809@gmail.com>
2023-10-16 21:31:53 +0900

    week01 ver.04

---
+++ week01/src/Pokemonster/Pickashu.java
@@ -0,0 +1,7 @@
+package Pokemonster;
+
+public class Pickashu extends Pokemon {
+    public void attack() {
+        System.out.println(this.name + "이(가) 10만 볼트를 사용합니다.");
+    }
+}
---
+++ week01/src/Pokemonster/Squirtle.java
@@ -0,0 +1,5 @@
+package Pokemonster;
+
+public class Squirtle extends Pokemon{
+
+}
